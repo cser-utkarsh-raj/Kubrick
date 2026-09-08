b65bb6380d0031bec76d61f77ee88f056d0c549b
UtkarshRaj <cser.utkarsh.raj@gmail.com>
2026-09-08 14:28:15 +0530

    feat: expose editorial take scoring

--- kubrick/editor/__init__.py
+++ kubrick/editor/__init__.py
@@ -1,4 +1,12 @@
 from .analyzer import AnalyzerConfig, analyze
 from .render import render
+from .takes import TakeScore, score_take, select_best_take
 
-__all__ = ["AnalyzerConfig", "analyze", "render"]
+__all__ = [
+    "AnalyzerConfig",
+    "TakeScore",
+    "analyze",
+    "render",
+    "score_take",
+    "select_best_take",
+]
