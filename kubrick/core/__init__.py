c075e4308c3feb26dc2831fba93098631f8e8aa2
UtkarshRaj <cser.utkarsh.raj@gmail.com>
2026-09-08 21:09:19 +0530

    feat(core): expose project and preset APIs

--- kubrick/core/__init__.py
+++ kubrick/core/__init__.py
@@ -3,6 +3,8 @@ from .editor import TimelinePlan, build_timeline
 from .editorial_plan import EditorialPolicy, decisions_from_findings
 from .models import AnalysisReport, DecisionKind, EditDecision, KeepSegment, TimeRange
 from .policy import EditingProfile, PROFILES
+from .presets import EditPreset, PRESETS, get_preset
+from .project import AudioClip, MediaClip, Overlay, Project
 from .silence import compress_pause, detect_silence_from_samples
 from .timeline import build_keep_segments
 
@@ -10,5 +12,6 @@ __all__ = [
     "AnalysisReport", "DecisionKind", "EditDecision", "KeepSegment", "TimeRange",
     "EditingProfile", "PROFILES", "SilencePolicy", "compress_pause",
     "detect_silence_from_samples", "build_keep_segments", "TimelinePlan", "build_timeline",
-    "EditorialPolicy", "decisions_from_findings",
+    "EditorialPolicy", "decisions_from_findings", "AudioClip", "MediaClip", "Overlay", "Project",
+    "EditPreset", "PRESETS", "get_preset",
 ]
