db91e78e761a4dae08a25f8926018862ef82b951
UtkarshRaj <cser.utkarsh.raj@gmail.com>
2026-09-08 14:27:00 +0530

    feat: expose visual analysis API

--- kubrick/media/__init__.py
+++ kubrick/media/__init__.py
@@ -1,3 +1,21 @@
 from .ffmpeg import FFmpegError, probe_duration, render_keep_segments
+from .visual import (
+    SceneChange,
+    VisualReport,
+    analyze_visuals,
+    detect_blackouts,
+    detect_frozen_frames,
+    detect_scene_changes,
+)
 
-__all__ = ["FFmpegError", "probe_duration", "render_keep_segments"]
+__all__ = [
+    "FFmpegError",
+    "SceneChange",
+    "VisualReport",
+    "analyze_visuals",
+    "detect_blackouts",
+    "detect_frozen_frames",
+    "detect_scene_changes",
+    "probe_duration",
+    "render_keep_segments",
+]
