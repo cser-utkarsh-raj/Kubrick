dd44301d1fd20730e56ec1178a4b57fe2228842a
UtkarshRaj <cser.utkarsh.raj@gmail.com>
2026-09-08 13:50:41 +0530

    refactor: expose canonical Kubrick editing pipeline

--- kubrick/core/__init__.py
+++ kubrick/core/__init__.py
@@ -1,19 +1,14 @@
 from .decisions import SilencePolicy
+from .editor import TimelinePlan, build_timeline
+from .editorial_plan import EditorialPolicy, decisions_from_findings
 from .models import AnalysisReport, DecisionKind, EditDecision, KeepSegment, TimeRange
 from .policy import EditingProfile, PROFILES
 from .silence import compress_pause, detect_silence_from_samples
 from .timeline import build_keep_segments
 
 __all__ = [
-    "AnalysisReport",
-    "DecisionKind",
-    "EditDecision",
-    "KeepSegment",
-    "TimeRange",
-    "EditingProfile",
-    "PROFILES",
-    "SilencePolicy",
-    "compress_pause",
-    "detect_silence_from_samples",
-    "build_keep_segments",
+    "AnalysisReport", "DecisionKind", "EditDecision", "KeepSegment", "TimeRange",
+    "EditingProfile", "PROFILES", "SilencePolicy", "compress_pause",
+    "detect_silence_from_samples", "build_keep_segments", "TimelinePlan", "build_timeline",
+    "EditorialPolicy", "decisions_from_findings",
 ]
