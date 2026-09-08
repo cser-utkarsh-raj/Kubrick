81b2f08f13fc4e0d390a92b785b917a0abffcfbd
UtkarshRaj <cser.utkarsh.raj@gmail.com>
2026-09-08 13:26:41 +0530

    refactor: expose core editing primitives

--- kubrick/core/__init__.py
+++ kubrick/core/__init__.py
@@ -1,1 +1,19 @@
-"""Core Kubrick domain and editing primitives."""
+from .decisions import SilencePolicy
+from .models import AnalysisReport, DecisionKind, EditDecision, KeepSegment, TimeRange
+from .policy import EditingProfile, PROFILES
+from .silence import compress_pause, detect_silence_from_samples
+from .timeline import build_keep_segments
+
+__all__ = [
+    "AnalysisReport",
+    "DecisionKind",
+    "EditDecision",
+    "KeepSegment",
+    "TimeRange",
+    "EditingProfile",
+    "PROFILES",
+    "SilencePolicy",
+    "compress_pause",
+    "detect_silence_from_samples",
+    "build_keep_segments",
+]
