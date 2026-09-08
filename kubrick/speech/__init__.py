c4c552d2f4cfb3e893924821851d926186effb28
UtkarshRaj <cser.utkarsh.raj@gmail.com>
2026-09-08 13:49:12 +0530

    feat: expose speech editorial analysis

--- kubrick/speech/__init__.py
+++ kubrick/speech/__init__.py
@@ -1,3 +1,15 @@
+from .editorial import EditorialConfig, EditorialFinding, build_editorial_findings, detect_fillers, detect_false_starts, detect_repetitions, pause_findings
 from .transcribe import SpeechSegment, Word, transcribe
 
-__all__ = ["SpeechSegment", "Word", "transcribe"]
+__all__ = [
+    "EditorialConfig",
+    "EditorialFinding",
+    "SpeechSegment",
+    "Word",
+    "build_editorial_findings",
+    "detect_fillers",
+    "detect_false_starts",
+    "detect_repetitions",
+    "pause_findings",
+    "transcribe",
+]
